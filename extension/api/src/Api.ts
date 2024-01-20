79346159bb3e63cb516a84e3cc25a1082ee5fb0b
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2024-01-21 00:43:44 +0200

    Add qs package and refactor API class
    Added qs package to the project dependencies for query string parsing. Also, significantly refactored the API class in `Api.ts` to include new types, helper functions and updated methods for handling and refreshing authentication tokens. These changes provide a cleaner and more efficient way to build URLs and manage authorization tokens.

--- extension/api/src/Api.ts
+++ extension/api/src/Api.ts
@@ -1,7 +1,31 @@
 import { Bridge } from '@shikivost/bridge';
+import queryString from 'qs';
 
 const clientId = 'ZYV_3N5DBDQWDhJYbWUq1YMcatv9nUI-xG51xsaXGAA';
 const clientSecret = 'vLW-Ppm52Qcel50kyXDLp0GxKt6Uc7xMahaLAHskNFg';
+
+export type Stringifiable = string | boolean | number | null | undefined;
+
+export type QueryObject = Record<
+  string,
+  Stringifiable | readonly Stringifiable[]
+>;
+
+export type CustomHeaders = { [key: string]: string | undefined };
+export type RequestOptions = Omit<RequestInit, 'headers'> & {
+  headers?: CustomHeaders;
+};
+
+export function buildUrl(
+  url: string,
+  params: QueryObject = {},
+  options: queryString.IStringifyOptions | undefined = undefined
+): string {
+  let query = queryString.stringify(params, options);
+  query = query ? `?${query}` : '';
+
+  return `https://shikimori.one${url}${query}`;
+}
 
 export class Api {
   private _accessToken: string;
@@ -26,7 +50,7 @@ export class Api {
     form.append('code', code);
     form.append('redirect_uri', 'https://animevost.org/');
 
-    const resp = await fetch('https://shikimori.one/oauth/token', {
+    const resp = await fetch(buildUrl('/oauth/token'), {
       method: 'POST',
       headers: {
         'User-Agent': 'Shikivost',
@@ -68,13 +92,19 @@ export class Api {
     form.append('refresh_token', this._refreshToken);
     form.append('redirect_uri', 'https://animevost.org/');
 
-    const resp = await fetch('https://shikimori.one/oauth/token', {
+    const resp = await fetch(buildUrl('/oauth/token'), {
       method: 'POST',
       headers: {
         'User-Agent': 'Shikivost',
       },
       body: form,
     });
+
+    if (resp.status === 401) {
+      this._refreshToken = '';
+      this._accessToken = '';
+      throw new Error('Request failed');
+    }
 
     if (!resp.ok) {
       throw new Error('Request failed');
@@ -88,5 +118,45 @@ export class Api {
 
     await bridge.send('background.store.access_token', access_token);
     await bridge.send('background.store.refresh_token', refresh_token);
+  }
+
+  private headers(extraHeaders: CustomHeaders = {}, isPublic = false) {
+    const defaultHeaders = new Headers();
+    defaultHeaders.append('accept', '*/*');
+
+    defaultHeaders.append('User-Agent', 'Shikivost');
+    defaultHeaders.append('Content-Type', 'application/json');
+
+    if (this._accessToken) {
+      defaultHeaders.append('Authorization', `Bearer ${this._accessToken}`);
+    }
+
+    Object.entries(extraHeaders).forEach(([key, value]) => {
+      if (value !== undefined) {
+        defaultHeaders.set(key, value);
+      } else {
+        defaultHeaders.delete(key);
+      }
+    });
+
+    return defaultHeaders;
+  }
+
+  async request(url: string, data: Partial<RequestOptions> = {}) {
+    const resp = await fetch(url, {
+      ...data,
+      headers: this.headers(data.headers),
+    });
+
+    if (resp.status == 401) {
+      await this.updateToken();
+      return this.request(url, data);
+    }
+
+    if (!resp.ok) {
+      throw new Error('Request failed');
+    }
+
+    return resp.json();
   }
 }
