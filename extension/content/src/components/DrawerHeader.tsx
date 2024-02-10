647ac2f53fd66c38b4f0cebdac75696ac32388cd
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2024-02-10 16:12:26 +0200

    Adjust CSS padding and add DrawerHeader component
    This commit removes padding from the CSS and adds a new file, DrawerHeader.tsx. The new DrawerHeader component is designed for use within a drawer layout, featuring a centrally aligned title and close button.

---
+++ extension/content/src/components/DrawerHeader.tsx
@@ -0,0 +1,43 @@
+export function DrawerHeader({ children, onClose }) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+        borderBottom: '1px solid grey',
+        padding: '8px 8px 12px',
+      }}
+    >
+      <span
+        style={{
+          fontSize: '18px',
+          display: 'block',
+          marginRight: '24px',
+        }}
+      >
+        {children}
+      </span>
+      <button
+        type="button"
+        onClick={onClose}
+        style={{
+          position: 'absolute',
+          right: '8px',
+          top: '8px',
+          width: '24px',
+          height: '24px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'white',
+          borderRadius: '4px',
+          border: '1px solid grey',
+        }}
+      >
+        x
+      </button>
+    </div>
+  );
+}
