ac8a142c29b598a6743d6ba405e0879fab0b3d5a
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2025-03-09 15:36:25 +0200

    Add CI workflow for building and releasing extensions

--- extension/static/generateManifest.js
+++ extension/static/generateManifest.js
@@ -11,7 +11,15 @@ if (args[2] === "--chrome") {
   data = require("./manifestFirefox.json");
 }
 
-data.version = packageJson.version;
+const ciVersion = process.env.VERSION;
+
+if (ciVersion && ciVersion.startsWith("v")) {
+  data.version = ciVersion.slice(1);
+} else {
+  data.version = packageJson.version;
+  console.warn("Using package.json version.");
+}
+
 data.description = packageJson.description;
 
 const pathToFile = "extension/static/assets/manifest.json";
